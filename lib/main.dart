087b1cfb8990b7fd99482f71ac0fcc22e707b413
virtualDestination <virtualdestination123@gmail.com>
2021-04-30 19:16:52 +0530

    fixing signIn

--- lib/main.dart
+++ lib/main.dart
@@ -238,7 +238,7 @@ class _MyAppState extends State<MyApp> {
             return SignIn();
           }
         }else{
-          return CircularProgressIndicator();
+          return SignIn();
         }
       }),
     );
